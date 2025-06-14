bbf71e0fca575693705ee91aef2191d4b2b9b9e2
baileynance <baieyj.nance@gmail.com>
2025-06-13 18:42:01 -0700

    connected server

--- handcrafted-haven/src/app/dashboard/products/page.tsx
+++ handcrafted-haven/src/app/dashboard/products/page.tsx
@@ -1,4 +1,9 @@
-export default function Products() {
+import { fetchProductList } from "@/app/lib/data";
+
+export default async function Products() {
+    const data = await fetchProductList();
+    console.log(data);
+
     return (
         <div>Product List</div>
     )
---
+++ handcrafted-haven/src/app/lib/data.ts
@@ -0,0 +1,33 @@
+import postgres from 'postgres';
+import {
+    ProductTableType,
+    ReviewTableType
+} from "@/app/lib/definitions";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+export async function fetchProductList() {
+    try {
+      const data = await sql<ProductTableType[]>`
+        SELECT products.id, products.name, products.description, products.image, products.price
+        FROM products`;
+
+      return data;
+    } catch (error) {
+      console.error('Database Error:', error);
+      throw new Error('Failed to fetch product list');
+    }
+}
+
+export async function fetchReviews() {
+    try {
+      const data = await sql<ReviewTableType[]>`
+        SELECT reviews.id, reviews.rating, reviews.review
+        FROM reviews`;
+
+      return data;
+    } catch (error) {
+      console.error('Database Error:', error);
+      throw new Error('Failed to fetch review');
+    }
+}
