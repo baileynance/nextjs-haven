47c6b485e8b94afb0a04bafcce39bf795916877c
baileynance <baieyj.nance@gmail.com>
2025-06-19 11:38:14 -0700

    change

--- handcrafted-haven/src/app/dashboard/account/page.tsx
+++ handcrafted-haven/src/app/dashboard/account/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/route";
+import { authOptions } from "@/app/api/auth/authOptions";
 
 export default async function AccountScreen() {
     const session = await getServerSession(authOptions);
